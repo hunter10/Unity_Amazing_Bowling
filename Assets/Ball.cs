d3bba5dee3ce0c25e590766b83bb3abc2bf71fec
hunter10 <hunter10@naver.com>
2019-06-01 23:59:22 +0900

    볼 슈터 제작

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public LayerMask whatIsProp;
     public ParticleSystem explosionParticle;
     public AudioSource explosionAudio;
 
@@ -19,6 +20,20 @@ public class Ball : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius, whatIsProp);
+
+        for(int i=0; i<colliders.Length; i++)
+        {
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+            targetRigidbody.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+
+            Prop targetProp = colliders[i].GetComponent<Prop>();
+
+            float damage = CalculateDamage(colliders[i].transform.position);
+
+            targetProp.TakeDamage(damage);
+        }
+
         explosionParticle.transform.parent = null;
 
         explosionParticle.Play();
@@ -26,5 +41,22 @@ public class Ball : MonoBehaviour
 
         Destroy(explosionParticle.gameObject, explosionParticle.main.duration);
         Destroy(gameObject);
+    }
+
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        Vector3 explosionToTarget = targetPosition - transform.position;
+
+        float distance = explosionToTarget.magnitude;
+
+        float edgeToCenterDistance = explosionRadius - distance;
+
+        float percentage = edgeToCenterDistance / explosionRadius;
+
+        float damage = maxDamage * percentage;
+
+        damage = Mathf.Max(0, damage);
+
+        return damage;
     }
 }
--- Assets/ShooterRotator.cs
+++ Assets/ShooterRotator.cs
@@ -14,37 +14,55 @@ public class ShooterRotator : MonoBehaviour
     public float verticalRotateSpeed = 360f;
     public float horizontalRotateSpeed = 360f;
 
+    public BallShooter ballShooter;
+
     private void Update()
     {
-        if(state == RotateState.Idle)
+
+
+        switch(state)
         {
-            if(Input.GetButtonDown("Fire1"))
-            {
-                state = RotateState.Horizontal;
-            }
-            
+            case RotateState.Idle:
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    state = RotateState.Horizontal;
+                }
+                break;
+
+            case RotateState.Horizontal:
+                if (Input.GetButton("Fire1"))
+                {
+                    transform.Rotate(new Vector3(0, horizontalRotateSpeed * Time.deltaTime, 0));
+                }
+                else if (Input.GetButtonUp("Fire1"))
+                {
+                    state = RotateState.Vertical;
+                }
+                break;
+
+            case RotateState.Vertical:
+                if (Input.GetButton("Fire1"))
+                {
+                    transform.Rotate(new Vector3(-verticalRotateSpeed * Time.deltaTime, 0, 0));
+                }
+                else if (Input.GetButtonUp("Fire1"))
+                {
+                    state = RotateState.Ready;
+                    ballShooter.enabled = true;
+                }
+                break;
+
+            case RotateState.Ready:
+                break;
         }
-        else if (state == RotateState.Horizontal)
-        {
-            if(Input.GetButton("Fire1"))
-            {
-                transform.Rotate(new Vector3(0, horizontalRotateSpeed * Time.deltaTime, 0));
-            }
-            else if(Input.GetButtonUp("Fire1"))
-            {
-                state = RotateState.Vertical;
-            }
-        }
-        else if(state == RotateState.Vertical)
-        {
-            if(Input.GetButton("Fire1"))
-            {
-                transform.Rotate(new Vector3(-verticalRotateSpeed * Time.deltaTime, 0, 0));
-            }
-            else if(Input.GetButtonUp("Fire1"))
-            {
-                state = RotateState.Ready;
-            }
-        }
+
+        
+    }
+
+    private void OnEnable()
+    {
+        transform.rotation = Quaternion.identity;
+        state = RotateState.Idle;
+        ballShooter.enabled = false;
     }
 }
